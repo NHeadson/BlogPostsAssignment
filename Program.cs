c5ef7aad7fe5408ffa63f707d5abbd33459c2850
Headsonn <nheadson@my.wctc.edu>
2024-11-05 13:00:25 -0600

    [1.07] Added Menu -Set up options 1 & 2 to work

--- Program.cs
+++ Program.cs
@@ -7,23 +7,46 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-// Create and save a new Blog
-Console.Write("Enter a name for a new Blog: ");
-var name = Console.ReadLine();
-
-var blog = new Blog { Name = name };
-
-var db = new DataContext();
-db.AddBlog(blog);
-logger.Info("Blog added - {name}", name);
-
-// Display all Blogs from the database
-var query = db.Blogs.OrderBy(b => b.Name);
-
-Console.WriteLine("All blogs in the database:");
-foreach (var item in query)
+string? choice;
+do
 {
-  Console.WriteLine(item.Name);
-}
+  // present the user with a menu
+  Console.WriteLine("1. Display all blogs");
+  Console.WriteLine("2. Add Blog");
+  Console.WriteLine("3. Create Post");
+  Console.WriteLine("4. Display Posts");
+
+  // get user input
+  Console.Write("Enter a number: ");
+  choice = Console.ReadLine();
+  logger.Info("User choice: {choice}", choice);
+
+  var db = new DataContext();
+
+  if (choice == "1")
+  {
+    // Display all Blogs from the database
+    var query = db.Blogs.OrderBy(b => b.Name);
+
+    Console.WriteLine("All blogs in the database:");
+    foreach (var item in query)
+    {
+      Console.WriteLine(item.Name);
+    }
+  }
+  else if (choice == "2")
+  {
+    // Create and save a new Blog
+    Console.Write("Enter a name for a new Blog: ");
+    var name = Console.ReadLine();
+
+    var blog = new Blog { Name = name };
+
+    db.AddBlog(blog);
+    logger.Info("Blog added - {name}", name);
+  
+  }
+} while (choice == "1" || choice == "2" || choice == "3" || choice == "4");
+
 
 logger.Info("Program ended");
